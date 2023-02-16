5122947ee1aded4e8980c7264abadf0bb185f424
axsylvia <771634986@qq.com>
2023-02-16 19:09:00 +0800

    init

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    hello login
+
+    <i class="iconfont icon-vue"></i>
+  </div>
+</template>
+
+<script setup lang="ts">
+/* import { defineComponent } from 'vue';
+defineComponent({
+  name: 'LoginView'
+}); */
+
+/* import http from '@/utils/http';
+http.post('/users/login', {
+    "email": "huangrong@imooc.com",
+    "pass": "huangrong"
+}).then((res)=>{
+  console.log(res.data);
+}) */
+
+import { useStore } from "@/store";
+
+const store = useStore();
+
+store.dispatch("users/login").then((res) => {
+  console.log(res.data);
+});
+</script>
+
+<style scoped lang="scss"></style>
